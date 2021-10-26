251c38e49ca07930ec7b1a046d2a6d0e59db5f9d
Parker King <kingparker.a@gmail.com>
2021-10-25 21:37:03 -0600

    Data models

--- src/main/java/Model/AppointmentRequest.java
+++ src/main/java/Model/AppointmentRequest.java
@@ -8,4 +8,44 @@ public class AppointmentRequest {
     private List<String> preferredDays;
     private List<Integer> preferredDocs;
     private Boolean isNew;
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer requestId) {
+        this.requestId = requestId;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public List<String> getPreferredDays() {
+        return preferredDays;
+    }
+
+    public void setPreferredDays(List<String> preferredDays) {
+        this.preferredDays = preferredDays;
+    }
+
+    public List<Integer> getPreferredDocs() {
+        return preferredDocs;
+    }
+
+    public void setPreferredDocs(List<Integer> preferredDocs) {
+        this.preferredDocs = preferredDocs;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
 }
